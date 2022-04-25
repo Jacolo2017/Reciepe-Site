535c0808c0dbdc2a3f4633106203ed92ff29d141
Jack <jacolo2017@gmail.com>
2022-04-25 17:39:15 -0600

    Tags messaround

--- tags/urls.py
+++ tags/urls.py
@@ -1,7 +1,20 @@
 from django.urls import path
 
-from tags.views import show_tags
+from tags.views import (
+    show_tags,
+    TagListView,
+    TagDetailView,
+    TagCreateView,
+    TagUpdateView,
+    TagDeleteView
+)
+
 
 urlpatterns = [
     path("", show_tags, name="tags_list"),
+    path("", TagListView.as_view(), name="tag_list"),
+    path("<int:pk>/", TagDetailView.as_view(), name="tag_detail"),
+    path("<int:pk>/delete/", TagDeleteView.as_view(), name="tag_delete"),
+    path("new/", TagCreateView.as_view(), name="tag_new"),
+    path("<int:pk>/edit/", TagUpdateView.as_view(), name="tag_edit"),
 ]
--- tags/views.py
+++ tags/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
+from django.views.generic.list import ListView
 
 try:
     from tags.models import Tag
+    from tags.forms import TagForm
+except Exception:
+    RecipeForm = None
+    Recipe = None
 except Exception:
     Tag = None
 
@@ -12,3 +20,34 @@ def show_tags(request):
         "tags": Tag.objects.all() if Tag else None,
     }
     return render(request, "tags/list.html", context)
+
+
+class TagListView(ListView):
+    model = Tag
+    template_name = "tags/list.html"
+    paginate_by = 10
+
+
+class TagDetailView(DetailView):
+    model = Tag
+    template_name = "tags/detail.html"
+
+
+class TagCreateView(CreateView):
+    model = Tag
+    template_name = "tags/new.html"
+    fields = ["name"]
+    success_url = reverse_lazy("tags_list")
+
+
+class TagUpdateView(UpdateView):
+    model = Tag
+    template_name = "tags/edit.html"
+    fields = ["name"]
+    success_url = reverse_lazy("tags_list")
+
+
+class TagDeleteView(DeleteView):
+    model = Tag
+    template_name = "tags/delete.html"
+    success_url = reverse_lazy("tags_list")
